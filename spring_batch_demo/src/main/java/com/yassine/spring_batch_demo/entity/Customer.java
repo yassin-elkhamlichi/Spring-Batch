e665c7f870dc6ac2987838c1354f1c500afa799f
yassin-elkhamlichi <yassinelkhamlichi98@gmail.com>
2025-12-15 14:32:23 +0100

    the csv file ready to proccess

---
+++ spring_batch_demo/src/main/java/com/yassine/spring_batch_demo/entity/Customer.java
@@ -0,0 +1,25 @@
+package com.yassine.spring_batch_demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.*;
+
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Customer {
+    @Id
+    @GeneratedValue
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String gender;
+    private String contactNo;
+    private String country;
+    private String dob;
+}
