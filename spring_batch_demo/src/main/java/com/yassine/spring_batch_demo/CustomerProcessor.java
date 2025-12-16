eb6221cd334c23d4e602f7a1232f327361321e05
yassin-elkhamlichi <yassinelkhamlichi98@gmail.com>
2025-12-16 14:48:11 +0100

    add the Customer Processor Logic

--- spring_batch_demo/src/main/java/com/yassine/spring_batch_demo/CustomerProcessor.java
+++ spring_batch_demo/src/main/java/com/yassine/spring_batch_demo/CustomerProcessor.java
@@ -1,12 +1,38 @@
 package com.yassine.spring_batch_demo;
 
 import com.yassine.spring_batch_demo.entity.Customer;
-import org.jspecify.annotations.Nullable;
 import org.springframework.batch.infrastructure.item.ItemProcessor;
 
-public class CustomerProcessor  implements ItemProcessor<Customer , Customer> {
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+
+public class CustomerProcessor implements ItemProcessor<Customer, Customer> {
+
+    public static int count = 0;
     @Override
-    public @Nullable Customer process(Customer item) throws Exception {
-        return item;
+    public Customer process(Customer customer) throws Exception {
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d-M-yyyy");
+
+        LocalDate dob = LocalDate.parse(customer.getDob(), formatter);
+        int age = Period.between(dob, LocalDate.now()).getYears();
+
+        if (age < 18) {
+            return null;
+        }
+        count++;
+        double originalBalance = customer.getBalance();
+        double bonus = originalBalance * 0.10;
+        double finalBalance = originalBalance + bonus;
+
+        customer.setBalance(finalBalance);
+
+        System.out.println("✅ Accepted: " + customer.getFirstName() +
+                " | Age: " + age +
+                " | Old Balance: " + originalBalance +
+                " | New Balance (+10%): " + finalBalance);
+        return customer;
     }
 }
